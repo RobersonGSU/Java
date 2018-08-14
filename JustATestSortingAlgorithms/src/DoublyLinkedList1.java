8dfc6529131f26380d75f27272e4af9a15aa29b3
thePrd1 <34972323+thePrd1@users.noreply.github.com>
2018-08-14 16:23:10 -0400

    java

--- JustATestSortingAlgorithms/src/DoublyLinkedList1.java
+++ JustATestSortingAlgorithms/src/DoublyLinkedList1.java
@@ -8,8 +8,61 @@ public class DoublyLinkedList1 {
 		DoublyLinkedNode newNode = new DoublyLinkedNode(data);
 		
 		newNode.setNextNode(this.head);
+		if(this.head !=null){
+			this.head.setPreviousNode(newNode);
+		}
+		
+		this.head= newNode;
 		
 		
 	}
 	
+	
+	public int getSize(){
+		int size =0;
+		
+		DoublyLinkedNode node = this.head;
+		
+		while(node !=null){
+			size++;
+			node = node.getNextNode();
+		}
+		
+		
+		
+		return size;
+		
+	}
+	
+
+
+
+@Override
+public String toString(){
+	String result = " { ";
+	
+	DoublyLinkedNode current = this.head;
+ 
+	
+	while(current !=null){
+		
+	 
+	 
+	 result += current.toString();
+	 if(current.getNextNode()!=null){
+		 result += " , ";
+	 }
+	 
+	current = current.getNextNode();
+		
+	}
+	
+	
+ 
+	result += " } ";
+	
+	
+	return result;
+}
+
 }
